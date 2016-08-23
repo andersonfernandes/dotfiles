802ed45b29abc17b48bf250f55165be8ff38cb24
Anderson Fernandes <fernandesanderson14@gmail.com>
2016-08-23 19:44:42 -0300

    Update battery.sh

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Shows the battery information with font awesome icons
+# Shows the battery information
 # Author: Anderson Fernandes <https://github.com/andersonfernandes>
 
 battery_0=""
@@ -12,19 +12,19 @@ percent=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep "percent
 
 state=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep "state:" | grep "discharging")
 if [$state -eq ""]; then
-	state="CHR"
+	state=" "
 else
-	state="DIS"
+	state=""
 fi
 
 if [ $percent -le 100 ] && [ $percent -gt 80 ]; then
-	echo  "$battery_4 $percent% $state"
+	echo  "$battery_4  $percent% $state"
 elif [ $percent -le 80 ] && [ $percent -gt 60 ]; then
-	echo "$battery_3 $percent% $state"
+	echo "$battery_3  $percent% $state"
 elif [ $percent -le 60 ] && [ $percent -gt 40 ]; then
-	echo "$battery_2 $percent% $state"
+	echo "$battery_2  $percent% $state"
 elif [ $percent -le 40 ] && [ $percent -gt 20 ]; then
-	echo "$battery_1 $percent% $state"
+	echo "$battery_1  $percent% $state"
 elif [ $percent -le 20 ] && [ $percent -gt 0 ]; then
-	echo "$battery_0 $percent% $state"
+	echo "$battery_0  $percent% $state"
 fi
