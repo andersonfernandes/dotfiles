f9def48aa09e406935b39d5e3af99543ae6657c8
Anderson Fernandes <fernandesanderson14@gmail.com>
2019-06-22 15:50:45 -0300

    Update tmux script
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- .scripts/open_tmux.sh
+++ .scripts/open_tmux.sh
@@ -5,12 +5,8 @@ if [ "$1" == "stant-web" ]; then
   tmux new-window -t stant-web -n console -c ~/Workspace/ruby/stant
   tmux new-window -t stant-web -n server -c ~/Workspace/ruby/stant
   tmux new-window -t stant-web -n assets -c ~/Workspace/ruby/stant
-  tmux new-window -t stant-web -n elixir-server -c ~/Workspace/elixir/stant-elixir-occurrences
 
   tmux send-keys -t stant-web:nvim "nvim" Enter
-  tmux send-keys -t stant-web:assets "yarn && yarn dev" Enter
-  tmux send-keys -t stant-web:server "rails server" Enter
-  tmux send-keys -t stant-web:elixir-server "mix phx.server" Enter
 
   tmux attach -t stant-web
 else
