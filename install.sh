d346f7321ad9605c7bf84829dc0037078814827e
Anderson Fernandes <fernandesanderson14@gmail.com>
2019-06-22 17:15:41 -0300

    Add install script
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Installing vim-plug"
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "Installing oh my zsh"
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
+export ZSH_THEME=avit
+export TERM=screen-256color
+
+echo "Linking config files"
+ln -s ~/.dotfiles/.tmux.conf ~/
+ln -s ~/.dotfiles/.config/nvim ~/.config/
+
+nvim +PlugInstall +qall > /dev/null
+
+echo "Done!"
