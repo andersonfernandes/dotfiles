ca4fe8af8cc131d36aeb69dfa48e10684a156417
Anderson Fernandes <fernandesanderson14@gmail.com>
2020-09-07 16:54:51 -0300

    Update install script
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+echo "Linking config files"
+ln -sfn ~/.dotfiles/.tmux.conf ~/
+ln -sfn ~/.dotfiles/.config/nvim ~/.config/
+
 echo "Installing vim-plug"
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo "Linking config files"
-ln -s ~/.dotfiles/.tmux.conf ~/
-ln -s ~/.dotfiles/.config/nvim ~/.config/
-
-nvim +PlugInstall +qall > /dev/null
+echo "Installing NeoVim plugins"
+nvim +'PlugInstall --sync' +qa
 
 cd ~/.fzf/ && ./install
 
