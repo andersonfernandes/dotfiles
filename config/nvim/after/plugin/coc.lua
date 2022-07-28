2928254cdc4f663097169c9b7933ebcd586bb4af
Anderson Fernandes <fernandesanderson14@gmail.com>
2022-07-27 23:13:21 -0300

    Add coc config
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- config/nvim/after/plugin/coc.lua
+++ config/nvim/after/plugin/coc.lua
@@ -13,4 +13,17 @@ vim.g.coc_global_extensions = {
   'coc-lua'
 }
 
+vim.cmd([[
+  function! s:check_back_space() abort
+    let col = col('.') - 1
+    return !col || getline('.')[col - 1]  =~# '\s'
+  endfunction
 
+  inoremap <silent><expr> <TAB>
+        \ pumvisible() ? coc#_select_confirm() :
+        \ coc#expandableOrJumpable() ? "\<C-r>=coc#rpc#request('doKeymap', ['snippets-expand-jump',''])\<CR>" :
+        \ <SID>check_back_space() ? "\<TAB>" :
+        \ coc#refresh()
+]])
+
+vim.g.coc_snippet_next = '<tab>'
