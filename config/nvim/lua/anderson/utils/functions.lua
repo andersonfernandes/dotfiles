26ac1345a3a01ad30780e427e9ecfc8cec2cbce3
Anderson Fernandes <fernandesanderson14@gmail.com>
2023-03-24 10:37:08 -0300

    Update toggle_git_status_cmd function to open git status in a vertical split
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- config/nvim/lua/anderson/utils/functions.lua
+++ config/nvim/lua/anderson/utils/functions.lua
@@ -2,7 +2,7 @@ function toggle_git_status_cmd()
     if string.find(vim.api.nvim_buf_get_name(0), 'fugitive:/') then
         vim.cmd(':bd')
     else
-        vim.cmd(':Git')
+        vim.cmd(':vertical Git')
     end
 end
 
